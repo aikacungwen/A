5729db27a06bf953ff2c11801885d597a90427b0
aika cungwen <70378095+aikacungwen@users.noreply.github.com>
2025-04-13 23:25:16 +0700

    Update worker.js

--- worker.js
+++ worker.js
@@ -1,19 +1,20 @@
 export default {
-  async fetch(request, env, ctx) {
+  async fetch(request) {
+    const url = new URL(request.url);
     const userAgent = request.headers.get("user-agent") || "";
     const isBot = /googlebot|bingbot|yandex|baiduspider/i.test(userAgent.toLowerCase());
 
     if (isBot) {
-      const prerenderUrl = `https://service.prerender.io${new URL(request.url).pathname}`;
-      const prerenderResponse = await fetch(prerenderUrl, {
+      const prerenderUrl = `https://service.prerender.io${url.pathname}`;
+      return fetch(prerenderUrl, {
         headers: {
           'X-Prerender-Token': 'Iuac5SW4SoEunFPRnNFA'
         }
       });
-      return prerenderResponse;
+    } else {
+      // Forward ke app.donghuafast.com
+      const realUrl = `https://app.donghuafast.com${url.pathname}`;
+      return fetch(realUrl, request);
     }
-
-    // Kalau bukan bot, teruskan request ke Cloudflare Pages
-    return fetch(request);
   }
 }
